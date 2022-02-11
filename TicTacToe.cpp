8d328d23cf63163fe143f55db4ab88307f6e34ab
Preston Windfeldt <prestonwindfeldt@gmail.com>
2022-02-11 16:12:51 -0700

    Place Marker

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <vector>
 
-int arr[3][3];
+char arr[3][3];
+
+struct Position {
+	int row;
+	int col;
+
+	Position() {
+		row = 0;
+		col = 0;
+	}
+
+	Position(int r, int c) {
+		row = r; 
+		col = c;
+	}
+
+	// already implemented for you!
+	bool operator==(const Position &other) {
+		return row == other.row && col == other.col;
+	}
+};
 
 void DisplayBoard() {
     for(int i=0;i<3;i++){
@@ -15,11 +35,14 @@ void DisplayBoard() {
 void fillarr(){
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            arr[i][j] = 0;
+            arr[i][j] = '-';
         }
     }
 }
 
+void PlaceMarker(Position p, char marker) {
+    arr[p.row][p.col] = marker;
+}
 
 int main(){
     fillarr();
